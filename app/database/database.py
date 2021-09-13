8f277cffb040dec0e3473ea423b5807960221367
mohammed shetaya <mohammedshetaya22@gmail.com>
2021-09-14 01:28:33 +0200

    project setup.

--- app/database/database.py
+++ app/database/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://CairoPulse:CairoPulse@cluster0.538hc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = MongoClient("put the database connection string here")
 db = client.cairo_pulse
 
 def get_neighborhoods_geometry () :

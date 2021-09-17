c9f9ff6d9c894a2a949fd4bcb5440e43f60cc179
MoamenShoman <moamenkshoman@gmail.com>
2021-09-17 15:27:54 +0200

    finished showing data upon click

--- Website/home-page-app.js
+++ Website/home-page-app.js
@@ -1,9 +1,7 @@
 function openNav(id) {
     document.getElementById("mySidenav").style.width = "500px";
-    document.getElementById("main").style.marginLeft = "0px";
 
-    if (id == "t") {
-        fetch('tagamoAwal.json')
+        fetch(id+".json")
         .then(function (response) {
             return response.json();
         })
@@ -13,55 +11,28 @@ function openNav(id) {
         .catch(function (err) {
             console.log('error: ' + err);
         });
-    } else if (id == "r") {
-        fetch('rehab.json')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            appendData(data);
-        })
-        .catch(function (err) {
-            console.log('error: ' + err);
-        });
+}
+
+function appendData(data) {
+    var PointsOfInterest= [];
+    let flag = false;
+    for(var x in data)
+    {
+        if(x.localeCompare("atms")==0)
+            flag=true;
+        if(flag)
+          PointsOfInterest.push(x);    
     }
-
-
-    
-   
-    function appendData(data) {
-   
-      
-        var PointsOfInterest= [];
-        let flag = false;
-        for(var x in data)
-        {
-            if(x.localeCompare("atms")==0)
-                flag=true;
-            if(flag)
-              PointsOfInterest.push(x);    
-        }
-
-    
-
-      for(let i=0;i<PointsOfInterest.length;i++){
-        var mainContainer = document.getElementById(PointsOfInterest[i]);
-        //   for (var i = 0; i < data.length; i++) {
-               var div = document.createElement("div");
-               
-               div.innerHTML = (data[PointsOfInterest[i]]).length;
-               mainContainer.innerHTML=div.innerHTML;
-       //    }
-      }
-    }
-
-
-
+  for(let i=0;i<PointsOfInterest.length;i++){
+    var mainContainer = document.getElementById(PointsOfInterest[i]);
+           var div = document.createElement("div");
+           div.innerHTML = (data[PointsOfInterest[i]]).length;
+           mainContainer.innerHTML=div.innerHTML;
+  }
 }
 
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
 }

caa59afa6101c7e39415af14bbbd855da8d995c3
Ahmed <elamory1999@gmail.com>
2021-09-19 23:13:44 +0200

    Leaflet Markers and PopUps Done

--- static/scripts/pin-map.js
+++ static/scripts/pin-map.js
@@ -1,3 +1,4 @@
+var markers = new Array();
 fields.forEach(function (poi) {
 
     const element_poi = document.getElementById(poi);
@@ -17,7 +18,29 @@ fields.forEach(function (poi) {
             })
         }) ;
         data = await response.json() ;
+        console.log(data);
+        data.forEach(function(point){
+            console.log("point");
+            console.log(point.location.coordinates);
+            var marker = L.marker([point.location.coordinates[1],point.location.coordinates[0]]);
+            // console.log(marker._icon);
+            // marker._icon.style.filter = "hue-rotate("+fields.indexOf(poi)*11+"deg)";
+            marker.bindPopup(point.name);
+            marker.on('mouseover', function (e) {
+                this.openPopup();
+            });
+            marker.on('mouseout', function (e) {
+                this.closePopup();
+            });
+            marker.addTo(map);
+        })
+        // console.log(marker._icon.style)
+
+
+
+
   });
 });
+
 
 

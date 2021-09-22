c5ba93b3f1c05c4d5063fb1084b3b3e4ed3862f9
mohammed shetaya <mohammedshetaya22@gmail.com>
2021-09-22 15:32:24 +0200

    deployment setup.

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gswx@#l(q3d_vu3#(a=6+#t+4y__@gmpp(&b2=1t8r)p@$z@6a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['127.0.0.1','https://cairopulse.herokuapp.com/']
 
 
 # Application definition
